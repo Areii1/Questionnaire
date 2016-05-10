a12ead324d120cabc9142e7dd60da4484f0ff59c
Areii1 <ari.jaaskelainen1@gmail.com>
2016-05-11 01:34:34 +0300

    Removed false rightAnswer keys from options.

--- questions.js
+++ questions.js
@@ -7,16 +7,13 @@ var questionsAndAnswers = [
         rightAnswer: true
       },
       {
-        label: 'Henrik Raitasola',
-        rightAnswer: false
+        label: 'Henrik Raitasola'
       },
       {
-        label: 'Linus Torvalds',
-        rightAnswer: false
+        label: 'Linus Torvalds'
       },
       {
-        label: 'Steve Ballmer',
-        rightAnswer: false
+        label: 'Steve Ballmer'
       }
     ]
   },
@@ -24,20 +21,17 @@ var questionsAndAnswers = [
     question: 'Which year was javascript released?',
     options: [
       {
-        label: '1999',
-        rightAnswer: false
+        label: '1999'
       },
       {
         label: '1995',
         rightAnswer: true
       },
       {
-        label: '2007',
-        rightAnswer: false
+        label: '2007'
       },
       {
-        label: '1976',
-        rightAnswer: false
+        label: '1976'
       }
     ]
   },
@@ -45,20 +39,17 @@ var questionsAndAnswers = [
     question: 'Which language is javascript mostly based on?',
     options: [
       {
-        label: 'Python',
-        rightAnswer: false
+        label: 'Python'
       },
       {
         label: 'C',
         rightAnswer: true
       },
       {
-        label: 'Clojure',
-        rightAnswer: false
+        label: 'Clojure'
       },
       {
-        label: 'Shabba',
-        rightAnswer: false
+        label: 'Shabba'
       }
     ]
   },
@@ -66,20 +57,17 @@ var questionsAndAnswers = [
     question: 'What is another name for Ecmascript 2015?',
     options: [
       {
-        label: 'AS782',
-        rightAnswer: false
+        label: 'AS782'
       },
       {
-        label: 'Clojure',
-        rightAnswer: false
+        label: 'Clojure'
       },
       {
         label: 'ES6',
         rightAnswer: true
       },
       {
-        label: '1976',
-        rightAnswer: false
+        label: '1976'
       }
     ]
   },
@@ -87,16 +75,13 @@ var questionsAndAnswers = [
     question: 'What is a popular javascript framework called?',
     options: [
       {
-        label: 'Nude.js',
-        rightAnswer: false
+        label: 'Nude.js'
       },
       {
-        label: 'IngularJS',
-        rightAnswer: false
+        label: 'IngularJS'
       },
       {
-        label: 'Clojure',
-        rightAnswer: false
+        label: 'Clojure'
       },
       {
         label: 'ReactJS',
