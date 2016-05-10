030b7e64b9356a9015c3f022e6aa0e4810f35991
Areii1 <ari.jaaskelainen1@gmail.com>
2016-05-10 23:16:26 +0300

    Created isRightAnswer() method, that checks if a submitted guess was correct.

--- questionnaireScript.js
+++ questionnaireScript.js
@@ -2,19 +2,28 @@ var form = document.getElementById('questionnaire');
 form.addEventListener('submit', function(e) {
 	e.preventDefault();
 
-	console.log("answer 1: " + getSelectedAnswerValue("answerType1"));
-	console.log("answer 2: " + getSelectedAnswerValue("answerType2"));
-	console.log("answer 3: " + getSelectedAnswerValue("answerType3"));
-	console.log("answer 4: " + getSelectedAnswerValue("answerType4"));
-	console.log("answer 5: " + getSelectedAnswerValue("answerType5"));
+  console.log('answer1 correct? ' + isRightAnswer(getGuess('answerType1')));
+  console.log('answer2 correct? ' + isRightAnswer(getGuess('answerType2')));
+  console.log('answer3 correct? ' + isRightAnswer(getGuess('answerType3')));
+  console.log('answer4 correct? ' + isRightAnswer(getGuess('answerType4')));
+  console.log('answer5 correct? ' + isRightAnswer(getGuess('answerType5')));
 });
 
-function getSelectedAnswerValue(answerType) {
+function getGuess(answerType) {
   var answerTypeElement = document.getElementsByName(answerType);
-  
+
   for (var i = 0; i < answerTypeElement.length; i++) {
     if (answerTypeElement[i].checked) {
         return selectedAnswerValue = answerTypeElement[i].value;
     }
+  }
+}
+
+function isRightAnswer(guess) {
+  if (guess.charAt(0) == '*') {
+    return true;
+  }
+  else {
+    return false;
   }
 }
