f6504186ca340ccd4b93ae07da25388bc280e8ed
Areii1 <ari.jaaskelainen1@gmail.com>
2016-05-11 00:55:31 +0300

    Refactored question creation process.

--- questionnaireScript.js
+++ questionnaireScript.js
@@ -50,16 +50,39 @@ function createQuestions() {
     console.log(questionsAndAnswers[i]);
     var question = questionsAndAnswers[i].question;
 
+    renderHeader(question);
+    renderOptions(questionsAndAnswers[i].options, i);
+  }
+}
+
+function renderHeader(question) {
     var questionHeader = document.createElement('h3');
     var questionHeaderText = document.createTextNode(question);
     questionHeader.appendChild(questionHeaderText);
-
     document.getElementById('questions').appendChild(questionHeader);
+}
 
-    for (var j = 0; j < questionsAndAnswers[i].options.length; j++) {
-      console.log(questionsAndAnswers[i].options[j]);
+function renderOptions(options, questionIndex) {
+    for (var i = 0; i < options.length; i++) {
+      console.log(options[i]);
+      var optionDiv = document.createElement('div');
+      var optionInput = document.createElement('input');
+      optionInput.type = 'radio';
+      optionInput.name = questionIndex;
+
+      if (options[i].rightAnswer) {
+        optionInput.value = '*' + questionIndex;
+      }
+      else {
+        optionInput.value = questionIndex;
+      }
+
+      var optionLabel = document.createTextNode(options[i].label);
+      optionDiv.appendChild(optionInput);
+      optionDiv.appendChild(optionLabel);
+
+      document.getElementById('questions').appendChild(optionDiv);
     }
-  }
 }
 
 var form = document.getElementById('questionnaire');
